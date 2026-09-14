ef2b8b50fec7cec00c8a1421b2d076bb24b8ae89
longpham58 <longngoc0910@gmail.com>
2026-09-14 21:28:02 +0700

    feat(dao) TODO3: implement todo 0.7 delete employee

--- Slot 2/Chapter1-JPA-Ex1/src/main/java/fu/de180871/dao/EmployeeDAO.java
+++ Slot 2/Chapter1-JPA-Ex1/src/main/java/fu/de180871/dao/EmployeeDAO.java
@@ -90,4 +90,22 @@ public class EmployeeDAO {
             em.close();
         }
     }
+
+    // ---------- DELETE (TODO 0.7) ----------
+    public void delete(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Employee e = em.find(Employee.class, id); // e dang MANAGED
+            if (e != null) {
+                em.remove(e); // -> e chuyen sang REMOVED, se bi DELETE khi commit
+            }
+            em.getTransaction().commit();
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
 }
