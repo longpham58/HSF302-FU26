f4ac450b2328fceef6a692aac7b7e9aeb74a3eca
longpham58 <longngoc0910@gmail.com>
2026-09-25 14:38:37 +0700

    feat(serviceImpl) : TODO 4.3 Creat ServiceImpl by Department and Student

--- Slot 6/ch4 (1)/ch4/src/main/java/com/hsf302/ch4/service/Impl/DepartmentServiceImpl.java
+++ Slot 6/ch4 (1)/ch4/src/main/java/com/hsf302/ch4/service/Impl/DepartmentServiceImpl.java
@@ -1,4 +1,19 @@
 package com.hsf302.ch4.service.Impl;
 
-public class DepartmentServiceImpl {
+import com.hsf302.ch4.repository.DepartmentRepository;
+import com.hsf302.ch4.repository.StudentRepository;
+import com.hsf302.ch4.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+    private final StudentRepository studentRepository;      // dùng ở TODO 22 (chuyển sinh viên)
+
+    // Các method được cài đặt dần từ TODO 6
 }
